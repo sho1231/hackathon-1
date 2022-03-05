8c0f57486f4ed062be0ae1228379c072fafd8e8b
sho1231 <38104743+sho1231@users.noreply.github.com>
2022-03-05 21:07:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,40 +18,53 @@ async function search(name){
         let del=document.querySelector(".table");
         del.remove();
     }
-    catch{
+    catch(err){
         console.log("catch");
     }
-    try
-    {let prom=await fetch(`https://api.nationalize.io?name=${name}`);
-    let obj=await prom.json();
-    // if(obj["country"][0]["country_id"]==undefined){
-    //     return;
-    // }
-    let table=document.createElement('table');
-    table.setAttribute('class','table table-md-3 table-sm-3 table-3');
-    table.innerHTML=`<thead class="thead-dark">
-    <tr>
-      <th >No.</th>
-      <th >Country Code</th>
-      <th >Probability value</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr style="background-color:grey">
-        <td>1</td>
-        <td>${obj["country"][0]["country_id"]}</td>
-        <td>${obj["country"][0]["probability"]}</td>
-    </tr>
-    <tr style="background-color:#80808052">
-        <td>2</td>
-        <td>${obj["country"][1]["country_id"]}</td>
-        <td>${obj["country"][1]["probability"]}</td>
-    </tr>
-  </tbody>
-  `
-  document.body.append(table);
+    try{
+        let del2=document.querySelector(".alert");
+        del2.remove();
     }
     catch(err){
-        alert("Error.......");
+        console.log("catch2");
+    }
+    try
+    {
+        let prom=await fetch(`https://api.nationalize.io?name=${name}`);
+        let obj=await prom.json();
+        let table=document.createElement('table');
+        table.setAttribute('class','table table-md-3 table-sm-3 table-3');
+        table.innerHTML=`<thead class="thead-dark">
+        <tr>
+        <th >No.</th>
+        <th >Country Code</th>
+        <th >Probability value</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr style="background-color:grey">
+            <td>1</td>
+            <td>${obj["country"][0]["country_id"]}</td>
+            <td>${obj["country"][0]["probability"]}</td>
+        </tr>
+        <tr style="background-color:#80808052">
+            <td>2</td>
+            <td>${obj["country"][1]["country_id"]}</td>
+            <td>${obj["country"][1]["probability"]}</td>
+        </tr>
+    </tbody>
+    `
+    document.body.append(table);
+    }
+    catch(err){
+        let div=document.createElement("div");
+        div.setAttribute('class','alert alert-danger alert-dismissible fade show');
+        div.setAttribute('role','alert');
+        div.innerHTML=`<strong>Error occured!</strong>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>`
+        document.body.append(div);
+        document.querySelector(".alert").alert('close');
     }
 }
